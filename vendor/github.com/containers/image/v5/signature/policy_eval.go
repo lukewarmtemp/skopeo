26520d81ce421c629501293443566b60b74b192d
Luke Yang <luyang@redhat.com>
2023-12-05 15:36:34 -0500

    wip

--- vendor/github.com/containers/image/v5/signature/policy_eval.go
+++ vendor/github.com/containers/image/v5/signature/policy_eval.go
@@ -287,7 +287,7 @@ func (pc *PolicyContext) IsRunningImageAllowed(ctx context.Context, publicImage 
 			return false, err
 		}
 		logrus.Debugf(" Requirement %d: allowed", reqNumber)
-		log.Println(" Requirement %d: allowed", reqNumber)
+		log.Printf(" Requirement %d: allowed", reqNumber)
 	}
 	// We have tested that len(reqs) != 0, so at least one req must have explicitly allowed this image.
 	logrus.Debugf("Overall: allowed")
