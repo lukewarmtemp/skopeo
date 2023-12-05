46023411773075d9e892800dad35c0f6d76ab6cb
Luke Yang <luyang@redhat.com>
2023-12-05 16:05:48 -0500

    wip

--- cmd/skopeo/proxy.go
+++ cmd/skopeo/proxy.go
@@ -269,6 +269,9 @@ func (h *proxyHandler) openImageImpl(args []any, allowNotFound bool) (retReplyBu
 	}
 
 	policyContext, err := h.opts.global.getPolicyContext()
+	log.Printf("Enter")
+	log.Printf("%+v\n", policyContext)
+	log.Printf("Exit")
 	if err != nil {
 		return ret, err
 	}
