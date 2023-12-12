f8d2caedad655b59dada35d58e492416eca613ed
Luke Yang <luyang@redhat.com>
2023-12-12 13:13:43 -0500

    wip

--- vendor/github.com/containers/image/v5/docker/docker_image_src.go
+++ vendor/github.com/containers/image/v5/docker/docker_image_src.go
@@ -467,7 +467,6 @@ func (s *dockerImageSource) getSignaturesFromLookaside(ctx context.Context, inst
 
 		sigURL := lookasideStorageURL(s.c.signatureBase, manifestDigest, i)
 		log.Printf("%v", sigURL)
-		sigURL = 
 		signature, missing, err := s.getOneSignature(ctx, sigURL)
 		log.Printf("%v", signature)
 		log.Printf("%v", missing)
